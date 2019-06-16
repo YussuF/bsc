58fe04305c13870b50e7422b30022cbf9e72db27
yussuf <j5sf23@web.de>
2019-06-16 15:23:48 +0200

    German text at home now, english below

--- src/Components/home.js
+++ src/Components/home.js
@@ -4,6 +4,23 @@ class Home extends React.Component {
     render() {
         return (
             <div>
+                <h2 className="front_head">Willkommen !</h2>
+                <p className="front_text">
+                    Dies ist das Classification Correction Helper Interface. Hier können die Fehler des neuronalen Netzes ausgemerzt werden, auf dass es lernt.
+                    <br/><br/>
+                    Ein Klick auf <i>classifier</i> oben rechts startet die Korrekturen. Nach Korrektur aller Gedichte wird das Interface bescheid geben.
+                    Allerdings können Änderungen auch nachträglich korrigiert werden (in diesem Falle bitte das popup ignorieren).
+                    Nebenbei, der <i>classifier</i> beinhaltet ein Balkendiagramm. Falls es nicht sichtbar sein sollte ist die Browserfensterweite zu gering.
+                    <br/><br/>
+                    <i>Categories</i> zeigt eine Übersicht über die vorhandenenen Kategorien, sowie aller in das Interface geladener Gedichte.
+                    Die Ansicht beinhaltet darüber hinaus die Optionen Kategorien hinzuzufügen, umzubenennen und (rudimentär) zu löschen.
+                    Außerdem können von dort aus einzelne Gedichte zur Korrektur angesteuert werden.
+                    <br/><br/>
+                    Diese Seite wurde von Jasper E. gebaut, alle Beschwerden bitte an <a href="mailto:2elbesha@informatik.uni-hamburg.de">ihn</a>.
+                </p>
+
+
+
                 <h2 className="front_head">Welcome !</h2>
                 <p className="front_text">
                     This is the Classification Correction Helper Interface.
@@ -17,7 +34,7 @@ class Home extends React.Component {
                     By clicking on <i>categories</i>, you can get an overview of the categories, as well as all loaded poems in this interface.
                     There you have the option to add and remove categories, as well as to change the classifications of self-selected poems.
                     <br/><br/>
-                    This page was built by jasper E. , all complaints should go to <a href="mailto:2elbesha@informatik.uni-hamburg.de">Him</a>
+                    This page was built by Jasper E. , all complaints should go to <a href="mailto:2elbesha@informatik.uni-hamburg.de">him</a>
                 </p>
             </div>
         );
